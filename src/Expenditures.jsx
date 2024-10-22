6d4c1fbe3038f40c3d184b7e36c7c5d660b74e6d
Jay Cortes <cortesjay1991@gmail.com>
2024-10-22 15:23:30 +0200

    Array for months has been created

--- src/Expenditures.jsx
+++ src/Expenditures.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { fetchExpenditures, createExpenditure } from './api';
+import months from './months'
 
 const Expenditures = ({ token }) => {
   const [expenditures, setExpenditures] = useState([]);
   const [amount, setAmount] = useState('');
   const [category, setCategory] = useState('');
   const [month, setMonth] = useState('');
+  const [expenditureFilter, setExpenditureFilter] = useState([]);
+
+  useEffect(() => {
+    setExpenditureFilter(expenditures)
+  }, [expenditures])
+
 
   const fetchData = async () => {
     try {
@@ -33,8 +40,23 @@ const Expenditures = ({ token }) => {
     }
   };
 
+  function handleFilterMonth(activeMonth) {
+    const result = expenditures.filter(expense => (
+      expense.month == activeMonth
+    ))
+    setExpenditureFilter(result)
+  };
+
   return (
     <div>
+      <div>
+        <h2>Months</h2>
+        <div>
+          {months.map((m)=> (
+            <button onClick={() => handleFilterMonth(m.date)} key={m.date}>{m.name}</button>
+          ))};
+        </div>
+      </div>
       <h2>Expenditures</h2>
       <form onSubmit={handleSubmit}>
         <input
@@ -51,22 +73,18 @@ const Expenditures = ({ token }) => {
           placeholder="Category"
           required
         />
-        <input
-          type="number"
-          value={month}
-          onChange={(e) => setMonth(e.target.value)}
-          placeholder="Month (1-12)"
-          required
-        />
+        <input type="month" onChange={(e) => setMonth(e.target.value)}
+          required min="2018-03" value={month} />
         <button type="submit">Add Expenditure</button>
       </form>
       <ul>
-        {expenditures.map((expenditure) => (
+        {expenditureFilter.map((expenditure) => (
           <li key={expenditure.id}>
             {expenditure.category}: {expenditure.amount} (Month: {expenditure.month})
           </li>
         ))}
       </ul>
+      <br />
     </div>
   );
 };
---
+++ src/months.js
@@ -0,0 +1,15 @@
+const months = [
+  { name: "January", date: "2024-01" },
+  { name: "February", date: "2024-02" },
+  { name: "March", date: "2024-03" },
+  { name: "April", date: "2024-04" },
+  { name: "May", date: "2024-05" },
+  { name: "June", date: "2024-06" },
+  { name: "July", date: "2024-07" },
+  { name: "August", date: "2024-08" },
+  { name: "September", date: "2024-09" },
+  { name: "October", date: "2024-10" },
+  { name: "November", date: "2024-11" },
+  { name: "December", date: "2024-12" }
+];
+export default months;
